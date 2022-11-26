36c8f6727f2b82b5f9a48ed9f38aae8c9d4a2cf1
rob duddy <rduddy13@gmail.com>
2022-11-25 20:14:30 -0500

    image file upload

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const usersRouter = require("./routes/users");
 const campsiteRouter = require("./routes/campsiteRouter");
 const promotionRouter = require("./routes/promotionRouter");
 const partnerRouter = require("./routes/partnerRouter");
+const uploadRouter = require('./routes/uploadRouter');
 
 const mongoose = require("mongoose");
 
@@ -56,6 +57,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/campsites", campsiteRouter);
 app.use("/promotions", promotionRouter);
 app.use("/partners", partnerRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
